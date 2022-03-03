29975be6d5fcf7d53e6e31876a72a8cc58f8e3e6
Jean Bueno Xavier <jbxavier@gmail.com>
2022-03-03 15:02:37 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,37 @@
+function tocaSom(idBotao) {
+    const elemento = document.querySelector(idBotao);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas.forEach(tipoTecla => {
+    const instrumento = tipoTecla.classList[1];
+
+    tipoTecla.onclick = () => {
+        const idSom = `#som_${instrumento}`;
+        tocaSom(idSom);
+    }    
+
+    tipoTecla.onkeydown = (evento) => {
+
+        console.log(evento);
+
+        if (evento.code === 'Enter' || evento.code === 'Space' || 
+            evento.code === 'NumpadEnter') {
+            tipoTecla.classList.add('ativa');
+        }
+        
+    }
+
+    tipoTecla.onkeyup = () => {
+        tipoTecla.classList.remove('ativa');
+    }
+});
+
